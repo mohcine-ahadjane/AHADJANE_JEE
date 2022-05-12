1f3e3d0a7609cadc9555a360b0cff91a2ba8855a
mohcine-ahadjane <mohcine-ahadjane@gmail.com>
2022-05-13 00:07:30 +0100

    n

--- jpa-ap/src/main/java/enset/ma/jpaap/JpaApApplication.java
+++ jpa-ap/src/main/java/enset/ma/jpaap/JpaApApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.Date;
 import java.util.List;
@@ -20,13 +23,25 @@ public class JpaApApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-patientRespository.save(new Patient(null, "Mohcine", new Date(), false, 56));
+/*patientRespository.save(new Patient(null, "Mohcine", new Date(), false, 56));
         patientRespository.save(new Patient(null, "Mohcine", new Date(), false, 56));
         patientRespository.save(new Patient(null, "Mohammed", new Date(), true, 67));
         patientRespository.save(new Patient(null, "Omar", new Date(), false, 80));
         patientRespository.save(new Patient(null, "Kamal", new Date(), false, 15));
-        List<Patient> patients = patientRespository.findAll();
-        patients.forEach(p->{
+     */
+        for (int i=0;i < 100; i++){
+            patientRespository.save(
+                    new Patient(null, "Mohcine", new Date(), Math.random()>0.5?true:false, (int) Math.random()*100));
+        }
+        Page<Patient> patients = patientRespository.findAll(PageRequest.of(5,5));
+        System.out.println("Total pages : "+patients.getTotalPages());
+        System.out.println("Total Elements : "+patients.getTotalElements());
+        System.out.println("Numbers of pages : " + patients.getNumber());
+        List<Patient> content = patients.getContent();
+        //List<Patient> byMalade = patientRespository.findPatientByMalade(true);
+        Page<Patient> byMalade = patientRespository.findByMalade(true, PageRequest.of(1,4));
+        List<Patient> patientList=patientRespository.chercherPatients("%h%", 40);
+        byMalade.forEach(p->{
             System.out.println("--------------------------------");
             System.out.println(p.getId());
             System.out.println(p.getNom());
@@ -40,7 +55,7 @@ patientRespository.save(new Patient(null, "Mohcine", new Date(), false, 56));
             System.out.println(patient.isMalade());
         }
         patient.setScore(89);
-        patientRespository.deleteById(1L);
+        //patientRespository.deleteById(1L);
 
     }
 }
--- jpa-ap/src/main/java/enset/ma/jpaap/repositories/PatientRepository.java
+++ jpa-ap/src/main/java/enset/ma/jpaap/repositories/PatientRepository.java
@@ -1,6 +1,23 @@
 package enset.ma.jpaap.repositories;
 import enset.ma.jpaap.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    public List<Patient> findByMalade(boolean m);
+    public Page<Patient> findByMalade(boolean m, Pageable pageable);
+    List<Patient> findByMaladeAndScoreLessThan(boolean m,int score);
+    List<Patient> findByMaladeIsTrueAndScoreLessThan(int score);
+    List<Patient> findByDateNaissanceBetweenAndMaladeIsTrueOrNomLike(Date d1, Date d2, String mc);
+    //@Query("select p from Patient p where p.dateNaissance between :x and :y or p.nom like :z")
+    //List<Patient> chercherPatients(@Param("x") Date d1, @Param("y") Date d2, @Param("z") String mc);
+    @Query("select p from Patient p where p.nom like :x and p.score >:y ")
+    List<Patient> chercherPatients(@Param("x") String nom, @Param("y") int scoreMin);
 }
