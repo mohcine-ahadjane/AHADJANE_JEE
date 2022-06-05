bed59e4ab0022276d89fa09daeaa1c36f54e6217
mohcine-ahadjane <mohcine-ahadjane@gmail.com>
2022-06-05 23:44:47 +0100

    commit

---
+++ digital-banking/digital-banking-web/src/main/java/ma/enset/digitalbankingweb/DigitalBankingWebApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.digitalbankingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingWebApplication.class, args);
+	}
+
+}
---
+++ digital-banking/digital-banking-web/src/test/java/ma/enset/digitalbankingweb/DigitalBankingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.digitalbankingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
